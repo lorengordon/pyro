845a1b65ab3e51f7899fc8e1af46313aa37370c9
Loren Gordon <loren@fleet-it.com>
2016-06-29 21:10:30 -0400

    Fix importerror when pandoc is missing

--- setup.py
+++ setup.py
@@ -18,12 +18,16 @@ try:
         pypandoc.convert('CHANGELOG.md', 'rst')
     ])
     long_description = long_description.replace("\r","")
-except OSError:
+except (ImportError, OSError):
     # pandoc is not installed, fallback to using raw contents
     print("Pandoc not found. Long_description conversion failure.")
     import io
     with io.open('README.md', encoding="utf-8") as f:
         long_description = f.read()
+
+setup_requires = [
+    'pypandoc',
+]
 
 setup(
     name='pyro',
@@ -68,6 +72,7 @@ setup(
     install_requires=[
         'click',
     ],
+    setup_requires=setup_requires,
     extras_require={
         # eg:
         #   'rst': ['docutils>=0.11'],
